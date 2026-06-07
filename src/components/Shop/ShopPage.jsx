5db9f7c5f835015ccabcff96a90946d6e0948cd1
NuRifat <najimullahrifat@gmail.com>
2026-06-07 12:26:09 +0600

    33.6-33.9 FilterSection Added

--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -1,52 +1,58 @@
-import { useState } from 'react';
-import ProductList from './ProductList';
-import Pagination from './Pagination';
-import useFetchProduct from '../../hooks/useFetchProducts';
+import { useState } from "react";
+import ProductList from "./ProductList";
+import Pagination from "./Pagination";
+import useFetchProduct from "../../hooks/useFetchProducts";
+import FilterSection from "./FilterSection";
+import useFetchCategories from "../../hooks/useFetchCategories";
 
 const ShopPage = () => {
-    // const [products, setProducts] = useState([]);
-    // const [loading, setLoading] = useState(false);
-    // const [totalPages, setTotalPages] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-    const {products, loading, totalPages} = useFetchProduct(currentPage);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
+  const [selectedCategory, setSelecetedCategory] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
 
-    // useEffect(() => {
-    //     fetchProducts();
-    // },[currentPage]);
+  const { products, loading, totalPages } = useFetchProduct(
+    currentPage,
+    priceRange,
+    selectedCategory,
+    searchQuery,
+    sortOrder
+  );
 
-    // const fetchProducts = () => {
-    //     setLoading(true);
-    //     apiClient
-    //         .get(`/products/?page=${currentPage}`)
-    //         .then((res) => {
-    //             setProducts(res.data.results);
-    //             setTotalPages(Math.ceil(res.data.count / res.data.results.length));
-    //         })
-    //         .catch((error) => console.log(error))
-    //         .finally(() => setLoading(false));
-    // }; // this one is not an ideal way, below is the good way
-    // const fetchProducts = async () => {
-    //     setLoading(true);
-    //     try {
-    //         const response = await apiClient.get(`/products/?page=${currentPage}`);
-    //         const data = await response.data;
+  const categories = useFetchCategories();
 
-    //         setProducts(data.results);
-    //         setTotalPages(Math.ceil(data.count / data.results.length));
-    //     } catch (error) {
-    //         console.log(error);
-    //     } finally {
-    //         setLoading(false);
-    //     }
-    // }; // even this one is not good, better way is to use a custom hook useFetchProduct
+  const handlePriceChange = (index, value) => {
+    setPriceRange((prev) => {
+      const newRange = [...prev];
+      newRange[index] = value;
+      return newRange;
+    });
+    setCurrentPage(1);
+  };
 
-
-    return (
-        <div className="max-w-7xl mx-auto px-4 py-8">
-            <ProductList products={products} loading={loading}/>
-            <Pagination totalPages={totalPages} currentPage={currentPage} handlePageChange={setCurrentPage}/>
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8">Shop Our Products</h1>
+      <FilterSection
+        priceRange={priceRange}
+        handlePriceChange={handlePriceChange}
+        categories={categories}
+        selectedCategory={selectedCategory}
+        handleCategoryChange={setSelecetedCategory}
+        searchQuery={searchQuery}
+        handleSearchQuery={setSearchQuery}
+        sortOrder={sortOrder}
+        handleSorting={setSortOrder}
+      />
+      <ProductList products={products} loading={loading} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        handlePageChange={setCurrentPage}
+      />
+    </div>
+  );
 };
 
 export default ShopPage;
