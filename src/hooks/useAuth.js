796ea848c7121494b30bbda66996e27587259de7
NuRifat <najimullahrifat@gmail.com>
2026-06-08 08:13:08 +0600

    34.4 Login hook-form added

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import apiClient from "../services/api-client";
+
+const useAuth = () => {
+  const [user, setUser] = useState(null);
+
+  const getToken = () => {
+    const token = localStorage.getItem("authTokens");
+    return token ? JSON.parse(token) : null;
+  };
+
+  const [authTokens, setAuthTokens] = useState(getToken());
+
+  // Login User
+  const loginUser = async (userData) => {
+    console.log(userData);
+    const response = await apiClient.post("/auth/jwt/create/", userData);
+    console.log(response.data);
+  };
+
+  return { user, loginUser };
+};
+
+export default useAuth;
