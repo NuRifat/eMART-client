8b2cc0a75e597efc553d52f5dffa09c0a2b0dd44
NuRifat <najimullahrifat@gmail.com>
2026-07-18 14:47:01 +0600

    Navbar z-index issue

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const { user, logoutUser } = useAuthContext();
   const { cart } = useCartContext();
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
